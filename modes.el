e8afac96db63cf89d84ed01e47c8a02e6f568da3
Upward Index <git@upwardindex.com>
2011-08-15 14:04:54 -0400

    Added clojure to paredit

--- modes.el
+++ modes.el
@@ -33,3 +33,4 @@
 (add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
 (add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
 (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'clojure-mode-hook (lambda () (paredit-mode +1)))
